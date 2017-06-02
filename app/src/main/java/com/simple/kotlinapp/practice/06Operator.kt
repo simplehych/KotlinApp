04e89c3e706eeb6f40a1ac23cf74248f0c519352
hych <simplehych@gmail.com>
2017-06-02 09:26:47 +0800

    元素操作符

--- app/src/main/java/com/simple/kotlinapp/practice/06Operator.kt
+++ app/src/main/java/com/simple/kotlinapp/practice/06Operator.kt
@@ -213,4 +213,95 @@ class `06Operator` {
         listOf(2, 4, 6, 8)
         list.mapNotNull { it * 2 }
     }
+
+    /**
+     * 元素操作符
+     */
+    fun operator4() {
+        val list = listOf(1, 2, 3, 4, 5, 6)
+
+        /**
+         * contains
+         * 集合中是否包含指定元素，有则返回true
+         */
+        list.contains(2)
+
+        /**
+         * elementAt
+         * 返回给定index对应的元素，如果index数组越界则会抛出IndexOutBoundsException
+         */
+        list.elementAt(1)
+
+        /**
+         * elementAtOrElse
+         * 返回给定index对应的元素，如果index数组越界则会根据给定函数返回默认值
+         */
+        list.elementAtOrElse(10, { 2 * it })
+
+        /**
+         * elementAtOrNull
+         * 返回给定index对应的元素，如果index数组越界则会返回null
+         */
+        list.elementAtOrNull(10)
+
+        /**
+         * first
+         * 返回符合给定函数条件的第一个元素
+         */
+        list.first { it % 2 == 0 }
+
+        /**
+         * firstOrNull
+         * 返回符合给定函数条件的第一个元素，如果没有符合则返回null
+         */
+        list.firstOrNull { it % 7 == 0 }
+
+        /**
+         * indexOf
+         * 返回指定元素的第一个index，如果不存在，则返回-1
+         */
+        list.indexOf(4)
+
+        /**
+         * indexOfFirst
+         * 返回第一个符合给定函数条件的元素的index，如果没有符合则返回-1
+         */
+        list.indexOfFirst { it % 2 == 0 }
+
+        /**
+         * indexOfLast
+         * 返回最后一个符合给定函数条件的元素的index，如果没有符合则返回-1
+         */
+        list.indexOfLast { it % 2 == 0 }
+
+        /**
+         * last
+         * 返回符合给定函数条件的最后一个元素
+         */
+        list.last { it % 2 == 0 }
+
+        /**
+         * lastIndexOf
+         * 返回指定元素的最后一个index，如果不存在，则返回-1
+         */
+        list.lastIndexOf(2)
+
+        /**
+         * lastOrNull
+         * 返回符合给定函数条件的最后一个元素，如果没有符合则返回null
+         */
+        list.lastOrNull { it % 7 == 0 }
+
+        /**
+         * single
+         * 返回符合给定函数的单个元素，如果没有挥着超过一个，则抛出异常
+         */
+        list.single { it % 5 == 0 }
+
+        /**
+         * singleOrNull
+         * 返回符合给定函数的单个元素，如果没有符合或者超过一个，则返回null
+         */
+        list.singleOrNull { it % 7 == 0 }
+    }
 }
